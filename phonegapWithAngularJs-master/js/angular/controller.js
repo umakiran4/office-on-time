50dc9cc56357f34164db42c47465124c192adfce
umakiran4 <critique.kiran@gmail.com>
2015-07-09 22:02:59 +0530

    Deleted
    Nt required

--- phonegapWithAngularJs-master/js/angular/controller.js
+++
@@ -1,7 +0,0 @@
-blogSample.controller('mainController', function($scope, $route){
-  
-    $scope.latitude = blogSample.latitude;
-    $scope.longitude = blogSample.longitude;
-                      
-});
-
