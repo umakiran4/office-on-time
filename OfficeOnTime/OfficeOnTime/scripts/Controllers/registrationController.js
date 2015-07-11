38c1c32d340ac50f2e2522c93428406027ba2bcd
umakiran4 <critique.kiran@gmail.com>
2015-07-11 23:37:02 +0530

    Geo location added

--- OfficeOnTime/OfficeOnTime/scripts/Controllers/registrationController.js
+++ OfficeOnTime/OfficeOnTime/scripts/Controllers/registrationController.js
@@ -1,6 +1,6 @@
 ﻿'use strict';
-ootApp.controller('RegistrationCtrl', ['$scope', '$http',
-      function ($scope, $http) {
+ootApp.controller('RegistrationCtrl', ['$scope', '$http','geolocation',
+      function ($scope, $http, geolocation) {
 
           $scope.User = {};
 
@@ -8,4 +8,17 @@ ootApp.controller('RegistrationCtrl', ['$scope', '$http',
               alert($scope.User.FirstName);
           }
 
+          $scope.getLocation = function () {
+              geolocation.getCurrentPosition(function (position) {
+                  alert('Latitude: ' + position.coords.latitude + '\n' +
+                        'Longitude: ' + position.coords.longitude + '\n' +
+                        'Altitude: ' + position.coords.altitude + '\n' +
+                        'Accuracy: ' + position.coords.accuracy + '\n' +
+                        'Altitude Accuracy: ' + position.coords.altitudeAccuracy + '\n' +
+                        'Heading: ' + position.coords.heading + '\n' +
+                        'Speed: ' + position.coords.speed + '\n' +
+                        'Timestamp: ' + position.timestamp + '\n');
+                  $scope.User.CheckInLocation = 'Latitude: ' + position.coords.latitude + '' + 'Longitude: ' + position.coords.longitude;
+              });
+          }
  }]);
--- OfficeOnTime/OfficeOnTime/scripts/index.js
+++ OfficeOnTime/OfficeOnTime/scripts/index.js
@@ -10,8 +10,8 @@
     function onDeviceReady() {
         // Handle the Cordova pause and resume events
         document.addEventListener( 'pause', onPause.bind( this ), false );
-        document.addEventListener( 'resume', onResume.bind( this ), false );
-        
+        document.addEventListener('resume', onResume.bind(this), false);
+
         // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
 
         window.onresize = resizeListIOS.bind(this, false);
