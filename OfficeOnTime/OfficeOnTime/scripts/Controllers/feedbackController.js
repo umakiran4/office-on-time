8fa8ebf7ced1920f85cde78c9dd0bb45c31d0314
anitha-kesari <anitha.kesari@gmail.com>
2015-07-12 16:30:49 +0530

    added star rating

--- OfficeOnTime/OfficeOnTime/scripts/Controllers/feedbackController.js
+++ OfficeOnTime/OfficeOnTime/scripts/Controllers/feedbackController.js
@@ -1,5 +1,35 @@
 ﻿'use strict';
-ootApp.controller('FeedbackCtrl', ['$scope',
-        function ($scope) {
+ootApp.controller('FeedbackCtrl', ['$scope', '$http',
+        function ($scope, $http) {
+            $scope.rating = {
+                current: 5,
+                max: 10
+            };
+            $scope.OnTime = { current: 5, max: 10 };
+            $scope.Driving = { current: 5, max: 10 };
+            $scope.Behaviour = { current: 5, max: 10 };
+            $scope.Hygenic = { current: 5, max: 10 };
+            $scope.Ratings = {}
+            $scope.getSelectedRating = function (rating, info) {
+                switch (info) {
+                    case "OnTime":
+                        $scope.Ratings.OnTime = rating;
+                        break;
+                    case "Driving":
+                        $scope.Ratings.Driving = rating;
+                        break;
+                    case "Behaviour":
+                        $scope.Ratings.Behaviour = rating;
+                        break;
+                    case "Hygenic":
+                        $scope.Ratings.Hygenic = rating;
+                        break;
+                    default:
+                        console.log(info);
+                }
+            }
 
+            $scope.Submit = function () {
+                alert($scope.Ratings.Driving);
+            }
         }]);
